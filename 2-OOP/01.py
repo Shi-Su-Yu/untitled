86a20d1b90bd65e3837e0dadcdfba3b609c625a8
xkwg <1966089049@qq.com>
2018-10-16 17:29:28 +0800

    Initial commit

---
+++ 2-OOP/01.py
@@ -0,0 +1,25 @@
+'''
+定义一个学生的类用来形容学生
+'''
+class Student():
+    pass
+
+#定义一个对象
+huang = ()
+class PythonStudent():
+    name = None
+    coure = "Python"
+
+    def doHomewok(self):
+        print("zuo zuoye")
+        return None
+yueyue = PythonStudent()
+print(yueyue.name)
+print(yueyue.coure)
+yueyue.doHomewok()
+print(yueyue.__dict__)
+
+
+
+
+
